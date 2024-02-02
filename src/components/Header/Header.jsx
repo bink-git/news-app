5bca0b7170bc0ef3b60bb033834ace82eaeb89c1
BINK <bink@email.ua>
2024-02-02 12:17:24 +0200

    git init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { formatDate } from '../../helpers/formatDate';
+import styles from './styles.module.css';
+
+const { header, title, date } = styles;
+const Header = () => {
+  return (
+    <header className={header}>
+      <h1 className={title}>News React</h1>
+      <p className={date}>{formatDate(new Date())}</p>
+    </header>
+  );
+};
+
+export default Header;
