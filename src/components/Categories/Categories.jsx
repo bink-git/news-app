58ebb47aa73645accd81779db2874bbee017e3a0
bink-git <bink@email.ua>
2024-12-25 14:32:33 +0100

    fix slider and banner scroll

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,46 +1,50 @@
+import { forwardRef } from 'react';
 import styles from './styles.module.css';
 
-const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
-  const colors = [
-    '#ffd0e0',
-    '#eebec3',
-    '#eed4db',
-    '#dac7eb',
-    '#bcd4e9',
-    '#d5f3d4',
-    '#f7dac6',
-  ];
-  return (
-    <div className={styles.categories}>
-      <button
-        onClick={() => setSelectedCategory(null)}
-        className={!selectedCategory ? styles.active : styles.item}
-      >
-        All
-      </button>
-      {categories.map((category, index) => {
-        const color = colors[index % colors.length];
-        const categoryCap =
-          category.charAt(0).toUpperCase() + category.slice(1);
-        return (
-          <button
-            key={category}
-            onClick={() => setSelectedCategory(category)}
-            className={
-              selectedCategory === category ? styles.active : styles.item
-            }
-            style={{
-              backgroundColor:
-                selectedCategory === category ? '#e7e7ff' : color,
-              color: selectedCategory === category ? '#6b4eff' : 'inherit',
-            }}
-          >
-            {categoryCap}
-          </button>
-        );
-      })}
-    </div>
-  );
-};
+const Categories = forwardRef(
+  ({ categories, setSelectedCategory, selectedCategory }, ref) => {
+    const colors = [
+      '#ffd0e0',
+      '#eebec3',
+      '#eed4db',
+      '#dac7eb',
+      '#bcd4e9',
+      '#d5f3d4',
+      '#f7dac6',
+    ];
+    return (
+      <div ref={ref} className={styles.categories}>
+        <button
+          onClick={() => setSelectedCategory(null)}
+          className={!selectedCategory ? styles.active : styles.item}
+        >
+          All
+        </button>
+        {categories.map((category, index) => {
+          const color = colors[index % colors.length];
+          const categoryCap =
+            category.charAt(0).toUpperCase() + category.slice(1);
+          return (
+            <button
+              key={category}
+              onClick={() => setSelectedCategory(category)}
+              className={
+                selectedCategory === category ? styles.active : styles.item
+              }
+              style={{
+                backgroundColor:
+                  selectedCategory === category ? '#e7e7ff' : color,
+                color: selectedCategory === category ? '#6b4eff' : 'inherit',
+              }}
+            >
+              {categoryCap}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+);
 
+Categories.displayName = 'Categories';
 export default Categories;
--- src/components/NewsFilters/NewsFilters.jsx
+++ src/components/NewsFilters/NewsFilters.jsx
@@ -2,6 +2,7 @@ import { getCategories } from '../../api/apiNews';
 import { useFetch } from '../../helpers/hooks/useFetch';
 import Categories from '../Categories/Categories';
 import Search from '../Search/Search';
+import Slider from '../Slider/Slider';
 import styles from './styles.module.css';
 
 const NewsFilters = ({ filters, changeFilters }) => {
@@ -10,13 +11,15 @@ const NewsFilters = ({ filters, changeFilters }) => {
   return (
     <div className={styles.filters}>
       {dataCategories && (
-        <Categories
-          categories={dataCategories.categories}
-          selectedCategory={filters.category}
-          setSelectedCategory={(category) =>
-            changeFilters('category', category)
-          }
-        />
+        <Slider>
+          <Categories
+            categories={dataCategories.categories}
+            selectedCategory={filters.category}
+            setSelectedCategory={(category) =>
+              changeFilters('category', category)
+            }
+          />
+        </Slider>
       )}
 
       <Search
